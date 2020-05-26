0e8b4867af52ad651bd5fa828b7244fe249248e2
Dom0798 <56403289+Dom0798@users.noreply.github.com>
2020-05-26 16:51:51 -0500

    Update IMU.cs

--- Uterus/Assets/Scrit/IMU.cs
+++ Uterus/Assets/Scrit/IMU.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -10,11 +10,8 @@ public class IMU : MonoBehaviour
 
     public GameObject target; // is the gameobject to
 
-    float acc_normalizer_factor = 0.00025f;
-    float gyro_normalizer_factor = 1.0f / 32768.0f;   // 32768 is max value captured during test on imu
-
-    float curr_angle_x = 92;
-    float curr_angle_y = 180;
+    float curr_angle_roll = 92;
+    float curr_angle_pitch = 180;
     float curr_angle_z = 0;
 
     float curr_offset_x = 0;
@@ -23,7 +20,6 @@ public class IMU : MonoBehaviour
 
     // Increase the speed/influence rotation
     public float factor = 7;
-
 
     public bool enableRotation;
     public bool enableTranslation;
@@ -76,39 +72,17 @@ public class IMU : MonoBehaviour
             // recived string is  like  "accx;accy;accz;gyrox;gyroy;gyroz"
             char splitChar = ';';
             string[] dataRaw = dataString.Split(splitChar);
-
+            
             // normalized accelerometer values
-            float ax = Int32.Parse(dataRaw[0]) * acc_normalizer_factor;
-            float ay = Int32.Parse(dataRaw[1]) * acc_normalizer_factor;
-            float az = Int32.Parse(dataRaw[2]) * acc_normalizer_factor;
-
-            // normalized gyrocope values
-            float gx = Int32.Parse(dataRaw[3]) * gyro_normalizer_factor;
-            float gy = Int32.Parse(dataRaw[4]) * gyro_normalizer_factor;
-            float gz = Int32.Parse(dataRaw[5]) * gyro_normalizer_factor;
-
-            // prevent
-            if (Mathf.Abs(ax) - 1 < 0) ax = 0;
-            if (Mathf.Abs(ay) - 1 < 0) ay = 0;
-            if (Mathf.Abs(az) - 1 < 0) az = 0;
-
-
-            curr_offset_x += ax;
-            curr_offset_y += ay;
-            curr_offset_z += 0; // The IMU module have value of z axis of 16600 caused by gravity
-
-
-            // prevent little noise effect
-            if (Mathf.Abs(gx) < 0.025f) gx = 0f;
-            if (Mathf.Abs(gy) < 0.025f) gy = 0f;
-            if (Mathf.Abs(gz) < 0.025f) gz = 0f;
-
-            curr_angle_x += gx;
-            curr_angle_y += gy;
-            curr_angle_z += gz;
-
-            if (enableTranslation) target.transform.position = new Vector3(curr_offset_x, curr_offset_z, curr_offset_y);
-            if (enableRotation) target.transform.rotation = Quaternion.Euler(curr_angle_x * factor, -curr_angle_z * factor, curr_angle_y * factor);
+            float roll = Int32.Parse(dataRaw[0]);
+            float pitch = Int32.Parse(dataRaw[1])
+            
+            curr_angle_roll += roll;
+            curr_angle_pitch += pitch;
+            curr_angle_yaw += 0;
+            
+            //Salida a objeto
+            if (enableRotation) target.transform.rotation = Quaternion.Euler(curr_angle_pitch * factor, 0, curr_angle_roll * factor);
         }
     }
 
