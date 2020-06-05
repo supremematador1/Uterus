952aca34563fa2daa4d93ca436f7190eda80b216
supremematador1 <A00820850@itesm.mx>
2020-06-05 17:12:23 -0500

    V2

---
+++ Uterus/Assets/Scrit/AToB.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AToB : MonoBehaviour
+{
+    public Animator animator;
+    float InputX;
+    public float InputY;
+    float InputZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get Animator
+        animator = this.gameObject.GetComponent<Animator>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+            InputY = Input.GetAxis("Vertical");
+            animator.SetFloat("InputY", InputY);
+        
+    }
+}
--- Uterus/Assets/Scrit/ArduinoMove.cs
+++ Uterus/Assets/Scrit/ArduinoMove.cs
@@ -1,28 +1,35 @@
-﻿using System.IO.Ports;
+<<<<<<< HEAD
+﻿using System.Collections;
 using UnityEngine;
+using System.IO.Ports;
+
 public class ArduinoMove : MonoBehaviour
 {
-    //private float yaw = 0.0f;
-    //private float pitch = 0.0f;
-    //public float HSpeed = 1.0f;
-    //public float VSpeed = 1.0f;
     public GameObject Cateter;
+    public GameObject Catetercito;
+    private float lerptime = 0.3f;
+    private float currentlerptime = 0;
     private double Registro = 1;
     private Vector3 startPos; //start
     private Vector3 endPos; //end
-    private float lerptime = 0.5f; //Time it will take to move
-    private float currentlerptime = 0; //This will update the lerp time
-    public static SerialPort sp = new SerialPort("COM4", 9600);
+    private Vector3 startPos2; //start
+    private Vector3 endPos2; //end
+    SerialPort sp = new SerialPort("COM6", 9600);
     // Start is called before the first frame update
     void Start()
     {
+        startPos = new Vector3(0.19f, 1.83f, -5.56f);
+        endPos = new Vector3(0.19f, 1.83f, -5.56f);
+        startPos2 = new Vector3(6.576f, 0.958f, 6.942f);
+        endPos2 = new Vector3(6.576f, 0.958f, 6.942f);
         sp.Open();
         sp.ReadTimeout = 1;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+
         if (sp.IsOpen)
         {
             try
@@ -30,93 +37,157 @@ public class ArduinoMove : MonoBehaviour
                 MoveObject(sp.ReadByte());
                 print(sp.ReadByte());
             }
-            catch (System.Exception)
+            catch
             {
 
             }
-        }
-
-        //////                 POR SI SE QUIERE CONTROLAR LA ORIENTACIÓN DEL OBJECTO CON EL MOUSE                ///////
-        //yaw += HSpeed * Input.GetAxis("Mouse X") * Time.deltaTime * 180;
-        //pitch -= VSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime * 180;
-        //yaw = Mathf.Clamp(yaw, -90f, 90f);//the rotation range
-        //pitch = Mathf.Clamp(pitch, -60f, 90f); //the rotation range
-        //transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-       
-
-        void MoveObject(int Direction)
-        {
-            if (Direction == 1 && Registro == 1)
-            {
-                startPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);
-                endPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);
-                // For movement transform.Translate(Vector3.left * amountmove, Space.Self);
-                //transform.position = new Vector3(0.14f, 1.206f, -4.267f);  //Versión 1.0
-                // Para rotation transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                Registro = 1;
-                currentlerptime = 0;
-            }
-            if (Direction == 1 && Registro == 2)
-            {
-                startPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);
-                endPos = Cateter.transform.position = new Vector3(0.14f, 1.206f, -4.26f);
-                // For movement transform.Translate(Vector3.left * amountmove, Space.Self);
-                //transform.position = new Vector3(0.14f, 1.206f, -4.267f);  //Versión 1.0
-                // Para rotation transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                Registro = 0;
-                currentlerptime = 0;
-            }
-            if (Direction == 1 && Registro == 2)
-            {
-                startPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);
-                endPos = Cateter.transform.position = new Vector3(0.14f, 1.206f, -4.26f);
-                // For movement transform.Translate(Vector3.left * amountmove, Space.Self);
-                //transform.position = new Vector3(0.14f, 1.206f, -4.267f);  //Versión 1.0
-                // Para rotation transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                Registro = 0;
-                currentlerptime = 0;
-            }
-            if (Direction == 2 && Registro == 1)
-            {
-                startPos = Cateter.transform.position = new Vector3(0.14f, 1.206f, -4.26f);//First position
-                endPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);//Second position
-                                                                                       // For movement transform.Translate(Vector3.up * amountmove, Space.Self);
-                                                                                       //transform.position = new Vector3(0.14f, 0.04f, 0.22f); //Versión1.0
-                Registro = 1;
-                currentlerptime = 0;
-            }
-            if (Direction == 2 && Registro == 3)
-            {
-                startPos = Cateter.transform.position = new Vector3(0.14f, -0.85f, 3.67f);//First position
-                endPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);//Second position
-                                                                                       // For movement transform.Translate(Vector3.up * amountmove, Space.Self);
-                                                                                       //transform.position = new Vector3(0.14f, 0.04f, 0.22f); //Versión1.0
-                Registro = 1;
-                currentlerptime = 0;
-            }
-            if (Direction == 3 & Registro == 2)
-            {
-                startPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);//First position
-                endPos = Cateter.transform.position = new Vector3(0.14f, -0.85f, 3.67f); //Second position
-                                                                                         // For movement transform.Translate(Vector3.right * amountmove, Space.Self);
-                                                                                         //transform.position = new Vector3(0.14f, -0.85f, 3.67f); //Versión1.0
-                Registro = 3;
-                currentlerptime = 0;
-            }
-            // SI QUISIERAS AGREGAR UN CUARTO SENSOR (4)
-            //if (Direction ==4)
-            //{
-            //   transform.position = new Vector3(0f, -7f, 21f);
-            //}
-            currentlerptime += Time.deltaTime;
-            if (currentlerptime >= lerptime)
-            {
-                currentlerptime = lerptime;
-            }
-            float Perc = currentlerptime / lerptime;
-            Cateter.transform.position = Vector3.Lerp(startPos, endPos, Perc);
         }
     }
+
+    void MoveObject(int Direction)
+    {
+        if (Direction == 1 && Registro == 2)
+        {
+            startPos = Cateter.transform.position = new Vector3(0.19f, 1.01f, -2.56f);
+            endPos = Cateter.transform.position = new Vector3(0.19f, 1.83f, -5.56f);
+            startPos2 = Catetercito.transform.position = new Vector3(7.35f, 1.37f, 6.44f);
+            endPos2 = Catetercito.transform.position = new Vector3(6.576f, 0.958f, 6.942f);
+            // For movement transform.Translate(Vector3.left * amountmove, Space.Self);
+            //transform.position = new Vector3(0.14f, 1.206f, -4.267f);  //Versión 1.0
+            // Para rotation transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            Registro = 1;
+            currentlerptime = 0;
+        }
+        if (Direction == 2 && Registro == 1)
+        {
+            startPos = Cateter.transform.position = new Vector3(0.19f, 1.83f, -5.56f);//First position
+            endPos = Cateter.transform.position = new Vector3(0.19f, 1.01f, -2.56f);//Second position
+            startPos2 = Catetercito.transform.position = new Vector3(6.576f, 0.958f, 6.942f);//First position
+            endPos2 = Catetercito.transform.position = new Vector3(7.35f, 1.37f, 6.44f);//Second position
+                                                                                        // For movement transform.Translate(Vector3.up * amountmove, Space.Self);
+                                                                                        //transform.position = new Vector3(0.14f, 0.04f, 0.22f); //Versión1.0
+            Registro = 2;
+            currentlerptime = 0;
+        }
+        if (Direction == 2 && Registro == 3)
+        {
+            startPos = Cateter.transform.position = new Vector3(0.19f, -0.13f, 1.74f);//First position
+            endPos = Cateter.transform.position = new Vector3(0.19f, 1.01f, -2.56f);//Second position
+            startPos2 = Catetercito.transform.position = new Vector3(8.14f, 1.79f, 5.92f);//First position
+            endPos2 = Catetercito.transform.position = new Vector3(7.35f, 1.37f, 6.44f);//Second position
+                                                                                        // For movement transform.Translate(Vector3.up * amountmove, Space.Self);
+                                                                                        //transform.position = new Vector3(0.14f, 0.04f, 0.22f); //Versión1.0
+            Registro = 2;
+            currentlerptime = 0;
+        }
+        if (Direction == 3 & Registro == 2)
+        {
+            startPos = Cateter.transform.position = new Vector3(0.19f, 1.01f, -2.56f);//First position
+            endPos = Cateter.transform.position = new Vector3(0.19f, -0.13f, 1.74f); //Second position
+            startPos2 = Catetercito.transform.position = new Vector3(7.35f, 1.37f, 6.44f);//First position
+            endPos2 = Catetercito.transform.position = new Vector3(8.14f, 1.79f, 5.92f); //Second position
+                                                                                         // For movement transform.Translate(Vector3.right * amountmove, Space.Self);
+                                                                                         //transform.position = new Vector3(0.14f, -0.85f, 3.67f); //Versión1.0
+            Registro = 3;
+            currentlerptime = 0;
+        }
+        currentlerptime += Time.deltaTime;
+        if (currentlerptime >= lerptime)
+        {
+            currentlerptime = lerptime;
+        }
+        float Perc = currentlerptime / lerptime;
+        Cateter.transform.position = Vector3.Lerp(startPos, endPos, Perc);
+        Catetercito.transform.position = Vector3.Lerp(startPos2, endPos2, Perc);
+    }
 }
+=======
+﻿using System.Collections;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoMove : MonoBehaviour
+{
+    public GameObject Cateter;
+    private float lerptime = 0.3f;
+    private float currentlerptime = 0;
+    private double Registro = 1;
+    private Vector3 startPos; //start
+    private Vector3 endPos; //end
+    SerialPort sp = new SerialPort("COM9", 9600);
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = new Vector3(0.14f, 1.206f, -4.26f);
+        endPos = new Vector3(0.14f, 1.206f, -4.26f);
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (sp.IsOpen)
+        {
+            try
+            {
+                MoveObject(sp.ReadByte());
+                print(sp.ReadByte());
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    void MoveObject(int Direction)
+    {
+        if (Direction == 1 && Registro == 2)
+        {
+            startPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);
+            endPos = Cateter.transform.position = new Vector3(0.14f, 1.206f, -4.26f);
+            // For movement transform.Translate(Vector3.left * amountmove, Space.Self);
+            //transform.position = new Vector3(0.14f, 1.206f, -4.267f);  //Versión 1.0
+            // Para rotation transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            Registro = 1;
+            currentlerptime = 0;
+        }
+        if (Direction == 2 && Registro == 1)
+        {
+            startPos = Cateter.transform.position = new Vector3(0.14f, 1.206f, -4.26f);//First position
+            endPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);//Second position
+                                                                                   // For movement transform.Translate(Vector3.up * amountmove, Space.Self);
+                                                                                   //transform.position = new Vector3(0.14f, 0.04f, 0.22f); //Versión1.0
+            Registro = 2;
+            currentlerptime = 0;
+        }
+        if (Direction == 2 && Registro == 3)
+        {
+            startPos = Cateter.transform.position = new Vector3(0.14f, -0.85f, 3.67f);//First position
+            endPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);//Second position
+                                                                                   // For movement transform.Translate(Vector3.up * amountmove, Space.Self);
+                                                                                   //transform.position = new Vector3(0.14f, 0.04f, 0.22f); //Versión1.0
+            Registro = 2;
+            currentlerptime = 0;
+        }
+        if (Direction == 3 & Registro == 2)
+        {
+            startPos = Cateter.transform.position = new Vector3(0.14f, 0.04f, 0.22f);//First position
+            endPos = Cateter.transform.position = new Vector3(0.14f, -0.85f, 3.67f); //Second position
+                                                                                     // For movement transform.Translate(Vector3.right * amountmove, Space.Self);
+                                                                                     //transform.position = new Vector3(0.14f, -0.85f, 3.67f); //Versión1.0
+            Registro = 3;
+            currentlerptime = 0;
+        }
+        currentlerptime += Time.deltaTime;
+        if (currentlerptime >= lerptime)
+        {
+            currentlerptime = lerptime;
+        }
+        float Perc = currentlerptime / lerptime;
+        Cateter.transform.position = Vector3.Lerp(startPos, endPos, Perc);
+    }
+}
+>>>>>>> 657863836e17611d07d84557674247aab616d309
